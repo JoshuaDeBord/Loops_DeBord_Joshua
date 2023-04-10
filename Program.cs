77a041729740c6f9de03671187d1cf29ac21b3b4
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-04-10 15:12:25 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Loops_DeBord_Joshua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Why do you want to make games?");
+            string answer1 = Console.ReadLine();
+            Console.WriteLine($"The sentence, \"{answer1}\" has {CountNumberOfSpaces(answer1)} spaces in it.");
+        }
+        static int CountNumberOfSpaces(string sentence)
+        {
+            int numberOfSpaces = 0;
+            foreach(char character in sentence)
+            {
+                if(character == ' ')
+                {
+                    numberOfSpaces++;
+                }
+            }
+            return numberOfSpaces;
+        }
+
+        static int SumOfDigits(string number)
+        {
+            int sum = 0;
+            foreach (char digit in number)
+            {
+                sum += (int)Char.GetNumericValue(digit);
+            }
+
+            )
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Loops_DeBord_Joshua
+ yuio;jh,vjnkbhjknlgkfhbcgjkghjhdfgkuff djgfcsgadagfdakh djkwkfegfv gafg jadcgvsf kahvk hdshgsdvuz mfsdhfgskdjxcrf.
